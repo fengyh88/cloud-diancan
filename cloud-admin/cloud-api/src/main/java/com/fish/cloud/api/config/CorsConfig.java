d3264166b86791c683de422f1408c31fbd78022f
fengyh880@163.com <fengyh88@126.com>
2021-01-20 08:55:55 +0800

    a

--- cloud-admin/cloud-api/src/main/java/com/fish/cloud/api/config/CorsConfig.java
+++ cloud-admin/cloud-api/src/main/java/com/fish/cloud/api/config/CorsConfig.java
@@ -4,12 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * @Description 跨域配置
- * @Author fengyh
- * @Date 2020/3/8 19:00
- * @Version 1.0
- */
 @Configuration
 public class CorsConfig extends WebMvcConfigurerAdapter {
     @Override
